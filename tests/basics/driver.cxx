55335d2067c625d52354dcfd57f42d2ed09ab323
helmesjo <helmesjo@gmail.com>
2022-04-26 19:54:52 +0200

    Removed default-generated header & cpp.

--- tests/basics/driver.cxx
+++ tests/basics/driver.cxx
@@ -1,8 +1,5 @@
-#include <sstream>
-#include <stdexcept>
-
 #include <threadable/version.hxx>
-#include <threadable/threadable.hxx>
+//#include <threadable/threadable.hxx>
 
 #undef NDEBUG
 #include <cassert>
@@ -10,26 +7,11 @@
 int main ()
 {
   using namespace std;
-  using namespace threadable;
+  //using namespace threadable;
 
   // Basics.
   //
-  {
-    ostringstream o;
-    say_hello (o, "World");
-    assert (o.str () == "Hello, World!\n");
-  }
-
-  // Empty name.
-  //
-  try
-  {
-    ostringstream o;
-    say_hello (o, "");
-    assert (false);
-  }
-  catch (const invalid_argument& e)
-  {
-    assert (e.what () == string ("empty name"));
-  }
+  // {
+  //   assert (o.str () == "Hello, World!\n");
+  // }
 }
