670e7b5c53e78b60b6361d880d739a82546f9770
helmesjo <helmesjo@gmail.com>
2022-04-27 02:30:02 +0200

    Added google-benchmark dependency and set up some rudimentary comparisons between job_queue, std::function & std::function + std::queue.

---
+++ threadable/job_queue.benchmark.cxx
@@ -0,0 +1,51 @@
+#include <threadable/job_queue.hxx>
+#include <benchmark/benchmark.h>
+
+#include <functional>
+#include <queue>
+
+static void job_queue(benchmark::State& state)
+{
+  threadable::job_queue queue;
+  for (auto _ : state)
+  {
+    int val = 0;
+    queue.push([&val]{ ++val; });
+    auto& job = queue.pop();
+    job();
+    benchmark::DoNotOptimize(job);
+    benchmark::DoNotOptimize(val);
+  }
+}
+
+static void std_function(benchmark::State& state)
+{
+  for (auto _ : state)
+  {
+    int val = 0;
+    auto job = std::function<void()>{[&val]{ ++val; }};
+    job();
+    benchmark::DoNotOptimize(job);
+    benchmark::DoNotOptimize(val);
+  }
+}
+static void std_queue(benchmark::State& state)
+{
+  std::queue<std::function<void()>> queue;
+  for (auto _ : state)
+  {
+    int val = 0;
+    queue.emplace([&val]{ ++val; });
+    auto& job = queue.front();
+    queue.pop();
+    job();
+    benchmark::DoNotOptimize(job);
+    benchmark::DoNotOptimize(val);
+  }
+}
+
+BENCHMARK(job_queue);
+BENCHMARK(std_function);
+BENCHMARK(std_queue);
+
+BENCHMARK_MAIN();
