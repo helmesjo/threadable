3ea4f13c810fb7726475a8c70425243a46067d4b
helmesjo <helmesjo@gmail.com>
2022-05-23 23:24:23 +0200

    pool: Replaced 'std::semaphore' with an atomic counter. Improved performance & also std::semaphore isn't available in anything other than very new compiler versions.

--- threadable/pool.hxx
+++ threadable/pool.hxx
@@ -6,7 +6,6 @@
 #include <concepts>
 #include <cstddef>
 #include <iterator>
-#include <semaphore>
 #include <thread>
 
 #define FWD(...) ::std::forward<decltype(__VA_ARGS__)>(__VA_ARGS__)
@@ -30,8 +29,20 @@ namespace threadable
           while(run_)
           {
             bool ranJob = false;
-            // aquire available jobs (decrement counter)
-            sem_.acquire();
+
+            std::size_t old;
+
+            // claim job as soon as it becomes available
+            do
+            {
+              old = readyCount_.load(std::memory_order_acquire);
+              if(old == 0)
+              {
+                readyCount_.wait(old);
+              }
+            }
+            while(old == 0 || !readyCount_.compare_exchange_weak(old, old-1, std::memory_order_release));
+
             auto queues = std::atomic_load_explicit(&queues_, std::memory_order_acquire);
             for(auto& queue : *queues)
             {
@@ -44,7 +55,9 @@ namespace threadable
             }
             if(!ranJob)
             {
-              sem_.release();
+              // if we never ran a job, we need to reverse our decrement
+              readyCount_.fetch_add(1, std::memory_order_release);
+              readyCount_.notify_one();
             }
           }
         });
@@ -58,7 +71,10 @@ namespace threadable
       {
         queue->quit();
       }
-      sem_.release(threads_.size());
+
+      // release all waiting threads
+      readyCount_.fetch_add(threads_.size(), std::memory_order_release);
+      readyCount_.notify_all();
       for(auto& thread : threads_)
       {
         thread.join();
@@ -67,12 +83,14 @@ namespace threadable
 
     auto create(execution_policy policy = threadable::execution_policy::concurrent) noexcept
     {
+      // make a new list of queues, copy from old list & append new queue, then atomically swap
       auto oldQueues = std::atomic_load_explicit(&queues_, std::memory_order_acquire);
       auto newQueues = std::make_shared<queues_t>();
       std::copy(std::cbegin(*oldQueues), std::cend(*oldQueues), std::back_inserter(*newQueues));
       newQueues->emplace_back(std::make_shared<queue_t>(policy, [this](...){
         // whenever a new job is pushed, release a thread (increment counter)
-        sem_.release();
+        readyCount_.fetch_add(1, std::memory_order_release);
+        readyCount_.notify_one();
       }));
       std::atomic_store_explicit(&queues_, newQueues, std::memory_order_release); 
       return newQueues->back();
@@ -86,10 +104,10 @@ namespace threadable
     }
 
   private:
+    std::atomic_bool run_{true};
+    std::atomic_size_t readyCount_{0};
     std::shared_ptr<queue<max_nr_of_jobs>> defaultQueue_;
     std::shared_ptr<queues_t> queues_ = std::make_shared<queues_t>();
-    std::counting_semaphore<max_nr_of_jobs> sem_{0};
-    std::atomic_bool run_{true};
     std::vector<std::thread> threads_;
   };
 }
