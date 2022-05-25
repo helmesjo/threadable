c3dff16a7c49da48a5f83d40a413817aced0a315
helmesjo <helmesjo@gmail.com>
2022-05-26 00:02:46 +0200

    benchmark: Made job/funcs even less optimizable, and fixed so that threadable::pool is made big enough to hold nr of jobs in benchmarks.

--- threadable/threadable.benchmark.cxx
+++ threadable/threadable.benchmark.cxx
@@ -9,14 +9,20 @@
 #include <queue>
 #include <vector>
 
+namespace
+{
+  int val;
+  constexpr std::size_t jobs_per_iteration = 4096;
+}
+
 static void threadable_function(benchmark::State& state)
 {
   using threadable_func_t = threadable::function<>;
   threadable_func_t func;
   for (auto _ : state)
   {
-    func = [val = 0]() mutable {
-      benchmark::DoNotOptimize(threadable::benchmark::do_work(val));
+    func = []() mutable {
+      benchmark::DoNotOptimize(val = threadable::benchmark::do_work(val));
     };
     func();
   }
@@ -28,8 +34,8 @@ static void std_function(benchmark::State& state)
   std::function<void()> func;
   for (auto _ : state)
   {
-    func = [val = 0]() mutable {
-      benchmark::DoNotOptimize(threadable::benchmark::do_work(val));
+    func = []() mutable {
+      benchmark::DoNotOptimize(val = threadable::benchmark::do_work(val));
     };
     func();
   }
@@ -41,8 +47,8 @@ static void threadable_queue(benchmark::State& state)
   threadable::queue queue;
   for (auto _ : state)
   {
-    queue.push([val = 0]() mutable {
-      benchmark::DoNotOptimize(threadable::benchmark::do_work(val));
+    queue.push([]() mutable {
+      benchmark::DoNotOptimize(val = threadable::benchmark::do_work(val));
     });
     auto job = queue.steal();
     job();
@@ -56,8 +62,8 @@ static void std_queue(benchmark::State& state)
 
   for (auto _ : state)
   {
-    queue.emplace([val = 0]() mutable {
-      benchmark::DoNotOptimize(threadable::benchmark::do_work(val));
+    queue.emplace([]() mutable {
+      benchmark::DoNotOptimize(val = threadable::benchmark::do_work(val));
     });
     auto& job = queue.front();
     job();
@@ -68,7 +74,7 @@ static void std_queue(benchmark::State& state)
 
 static void threadable_pool(benchmark::State& state, std::size_t nr_of_threads)
 {
-  auto pool = threadable::pool(nr_of_threads);
+  auto pool = threadable::pool<jobs_per_iteration>(nr_of_threads);
   std::vector<threadable::job_token> tokens;
   tokens.reserve(state.range(0));
 
@@ -76,8 +82,8 @@ static void threadable_pool(benchmark::State& state, std::size_t nr_of_threads)
   {
     for(std::int64_t i = 0; i < state.range(0); ++i)
     {
-      tokens.emplace_back(pool.push([val = 0]() mutable {
-        benchmark::DoNotOptimize(threadable::benchmark::do_work(val));
+      tokens.emplace_back(pool.push([]() mutable {
+        benchmark::DoNotOptimize(val = threadable::benchmark::do_work(val));
       }));
     }
 
@@ -103,8 +109,8 @@ static void std_no_pool(benchmark::State& state)
   {
     for(std::int64_t i = 0; i < state.range(0); ++i)
     {
-      queue.emplace_back([val = 0]() mutable {
-        benchmark::DoNotOptimize(threadable::benchmark::do_work(val));
+      queue.emplace_back([]() mutable {
+        benchmark::DoNotOptimize(val = threadable::benchmark::do_work(val));
       });
     }
 
@@ -126,7 +132,6 @@ BENCHMARK(std_function);
 BENCHMARK(threadable_queue);
 BENCHMARK(std_queue);
 
-static constexpr std::size_t jobs_per_iteration = 1024;
 BENCHMARK_CAPTURE(threadable_pool, threads: 1, 1)->Arg(jobs_per_iteration);
 BENCHMARK_CAPTURE(threadable_pool, threads: 2, 2)->Arg(jobs_per_iteration);
 BENCHMARK_CAPTURE(threadable_pool, threads: 3, 3)->Arg(jobs_per_iteration);
