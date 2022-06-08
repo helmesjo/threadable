be4e8402031a7bbff66db95c62e1a77b9887cb42
helmesjo <helmesjo@gmail.com>
2022-06-08 20:17:29 +0200

    benchmark: Removed (old) symbol exporting from benchmark utilities (it's now it's own executable-package, nothing to export).

--- threadable-benchmarks/threadable-benchmarks/util.hxx
+++ threadable-benchmarks/threadable-benchmarks/util.hxx
@@ -1,9 +1,7 @@
 #pragma once
 
-#include <threadable/export.hxx>
-
 namespace threadable::benchmark
 {
-  THREADABLE_SYMEXPORT int do_trivial_work(int& val);
-  THREADABLE_SYMEXPORT int do_non_trivial_work(int& val);
+  int do_trivial_work(int& val);
+  int do_non_trivial_work(int& val);
 }
