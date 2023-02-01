f650537aee12adb59b27a204e52c408f4e7e6740
helmesjo <helmesjo@gmail.com>
2023-02-01 14:50:59 +0100

    pool: Comment out benchmarks & tests.

--- threadable-benchmarks/threadable-benchmarks/pool.benchmark.cxx
+++ threadable-benchmarks/threadable-benchmarks/pool.benchmark.cxx
@@ -1,105 +1,105 @@
-#include <threadable/pool.hxx>
+// #include <threadable/pool.hxx>
 
-#include <threadable-benchmarks/util.hxx>
+// #include <threadable-benchmarks/util.hxx>
 
-#include <functional>
-#include <queue>
-#include <thread>
-#include <vector>
+// #include <functional>
+// #include <queue>
+// #include <thread>
+// #include <vector>
 
-namespace
-{
-  constexpr std::size_t jobs_per_iteration = 1 << 16;
-}
+// namespace
+// {
+//   constexpr std::size_t jobs_per_iteration = 1 << 16;
+// }
 
-static void pool_std_thread(benchmark::State& state)
-{
-  const std::size_t nr_of_jobs = state.range(0);
-  const std::size_t nr_of_threads = state.range(1);
-  std::vector<threadable::job_token> tokens;
-  tokens.reserve(nr_of_jobs);
+// static void pool_std_thread(benchmark::State& state)
+// {
+//   const std::size_t nr_of_jobs = state.range(0);
+//   const std::size_t nr_of_threads = state.range(1);
+//   std::vector<threadable::job_token> tokens;
+//   tokens.reserve(nr_of_jobs);
 
-  std::atomic_bool stop{false};
-  auto queue = std::make_shared<threadable::queue<jobs_per_iteration>>();
-  std::vector<std::thread> threads;
-  for(std::size_t i=0; i<nr_of_threads; ++i)
-  {
-    threads.emplace_back([&]{
-      while(!stop)
-      {
-        if(auto q = std::atomic_load(&queue))
-        {
-          while(auto job = q->steal())
-          {
-            benchmark::DoNotOptimize(job);
-          }
-        }
-      }
-    });
-  }
-  for (auto _ : state)
-  {
-    // pre-fill queue
-    auto q = std::make_shared<threadable::queue<jobs_per_iteration>>();
+//   std::atomic_bool stop{false};
+//   auto queue = std::make_shared<threadable::queue<jobs_per_iteration>>();
+//   std::vector<std::thread> threads;
+//   for(std::size_t i=0; i<nr_of_threads; ++i)
+//   {
+//     threads.emplace_back([&]{
+//       while(!stop)
+//       {
+//         if(auto q = std::atomic_load(&queue))
+//         {
+//           while(auto job = q->steal())
+//           {
+//             benchmark::DoNotOptimize(job);
+//           }
+//         }
+//       }
+//     });
+//   }
+//   for (auto _ : state)
+//   {
+//     // pre-fill queue
+//     auto q = std::make_shared<threadable::queue<jobs_per_iteration>>();
 
-    for(std::size_t i = 0; i < nr_of_jobs; ++i)
-    {
-      tokens.emplace_back(q->push([&]() mutable {}));
-    }
-    threadable::utils::time_block(state, [&]{
-      std::atomic_store(&queue, q);
-      for(const auto& token : tokens)
-      {
-        token.wait();
-      }
-    });
+//     for(std::size_t i = 0; i < nr_of_jobs; ++i)
+//     {
+//       tokens.emplace_back(q->push([&]() mutable {}));
+//     }
+//     threadable::utils::time_block(state, [&]{
+//       std::atomic_store(&queue, q);
+//       for(const auto& token : tokens)
+//       {
+//         token.wait();
+//       }
+//     });
 
-    tokens.clear();
-    q = nullptr;
-    std::atomic_store(&queue, q);
-  }
+//     tokens.clear();
+//     q = nullptr;
+//     std::atomic_store(&queue, q);
+//   }
 
-  stop = true;
-  for(auto& thread : threads)
-  {
-    thread.join();
-  }
+//   stop = true;
+//   for(auto& thread : threads)
+//   {
+//     thread.join();
+//   }
 
-  state.SetItemsProcessed(nr_of_jobs * state.iterations());
-}
+//   state.SetItemsProcessed(nr_of_jobs * state.iterations());
+// }
 
-static void pool_threadable(benchmark::State& state)
-{
-  const std::size_t nr_of_jobs = state.range(0);
-  const std::size_t nr_of_threads = state.range(1);
-  auto pool = threadable::pool<jobs_per_iteration>(nr_of_threads);
-  std::vector<threadable::job_token> tokens;
-  tokens.reserve(nr_of_jobs);
+// static void pool_threadable(benchmark::State& state)
+// {
+//   const std::size_t nr_of_jobs = state.range(0);
+//   const std::size_t nr_of_threads = state.range(1);
+//   auto pool = threadable::pool<jobs_per_iteration>(nr_of_threads);
+//   std::vector<threadable::job_token> tokens;
+//   tokens.reserve(nr_of_jobs);
 
-  for (auto _ : state)
-  {
-    // pre-fill queue
-    auto queue = std::make_shared<decltype(pool)::queue_t>(threadable::execution_policy::concurrent);
+//   for (auto _ : state)
+//   {
+//     // pre-fill queue
+//     auto queue = std::make_shared<decltype(pool)::queue_t>(threadable::execution_policy::concurrent);
 
-    for(std::size_t i = 0; i < nr_of_jobs; ++i)
-    {
-      tokens.emplace_back(queue->push([&]() mutable {}));
-    }
-    threadable::utils::time_block(state, [&]{
-      pool.add(queue);
+//     for(std::size_t i = 0; i < nr_of_jobs; ++i)
+//     {
+//       tokens.emplace_back(queue->push([&]() mutable {}));
+//     }
+//     threadable::utils::time_block(state, [&]{
+//       pool.add(queue);
 
-      for(const auto& token : tokens)
-      {
-        token.wait();
-      }
-    });
+//       for(const auto& token : tokens)
+//       {
+//         token.wait();
+//       }
+//     });
 
-    tokens.clear();
-    pool.remove(*queue);
-  }
+//     tokens.clear();
+//     pool.remove(*queue);
+//   }
   
-  state.SetItemsProcessed(nr_of_jobs * state.iterations());
-}
+//   state.SetItemsProcessed(nr_of_jobs * state.iterations());
+// }
 
-BENCHMARK(pool_threadable)->Args({jobs_per_iteration, 1})->ArgNames({"jobs", "threads"})->UseManualTime();
-BENCHMARK(pool_std_thread)->Args({jobs_per_iteration, 1})->ArgNames({"jobs", "threads"})->UseManualTime();
+// BENCHMARK(pool_threadable)->Args({jobs_per_iteration, 1})->ArgNames({"jobs", "threads"})->UseManualTime();
+// BENCHMARK(pool_std_thread)->Args({jobs_per_iteration, 1})->ArgNames({"jobs", "threads"})->UseManualTime();
--- threadable-tests/threadable-tests/pool.test.cxx
+++ threadable-tests/threadable-tests/pool.test.cxx
@@ -1,124 +1,124 @@
-#include <threadable/pool.hxx>
-#include <threadable-tests/doctest_include.hxx>
+// #include <threadable/pool.hxx>
+// #include <threadable-tests/doctest_include.hxx>
 
-#include <cstddef>
-#include <thread>
-#include <vector>
+// #include <cstddef>
+// #include <thread>
+// #include <vector>
 
-SCENARIO("pool: create/remove queues")
-{
-  auto pool = threadable::pool(1);
-  GIVEN("queue is created")
-  {
-    auto queue = pool.create();
-    int called = 0;
-    WHEN("job is pushed")
-    {
-      auto token = queue->push([&called]{ ++called; });
-      THEN("it gets executed")
-      {
-        token.wait();
-        REQUIRE(called == 1);
-      }
-    }
-    THEN("queue can be removed")
-    {
-      REQUIRE(pool.remove(*queue));
-    }
-  }
-  GIVEN("queue is pre-created")
-  {
-    auto queue = std::make_shared<decltype(pool)::queue_t>();
-    WHEN("added (without jobs) to pool")
-    {
-      REQUIRE(pool.add(queue));
-      AND_WHEN("job is pushed")
-      {
-        int called = 0;
-        auto token2 = queue->push([&called]{ ++called; });
-        THEN("it gets executed")
-        {
-          token2.wait();
-          REQUIRE(called == 1);
-        }
-      }
-      AND_WHEN("added to pool again")
-      {
-        THEN("it's not added")
-        {
-          REQUIRE_FALSE(pool.add(queue));
-        }
-      }
-      AND_WHEN("removed from pool")
-      {
-        REQUIRE(pool.remove(*queue));
-        THEN("it can be readded")
-        {
-          REQUIRE(pool.add(queue));
-        }
-      }
-    }
-    WHEN("added (with jobs) to pool")
-    {
-      THEN("existing jobs are executed")
-      {
-        int called = 0;
-        auto token = queue->push([&called]{ ++called; });
-        REQUIRE(pool.add(queue));
-        token.wait();
-        REQUIRE(called == 1);
-      }
-      AND_WHEN("removed from pool")
-      {
-        int called = 0;
-        std::atomic_int latch{2};
-        auto token1 = queue->push([&called, &latch]{ --latch; while(latch != 0); ++called; });
-        auto token2 = queue->push([&called]{ ++called; });
-        REQUIRE(pool.add(queue));
-        while(latch > 1);
+// SCENARIO("pool: create/remove queues")
+// {
+//   auto pool = threadable::pool(1);
+//   GIVEN("queue is created")
+//   {
+//     auto queue = pool.create();
+//     int called = 0;
+//     WHEN("job is pushed")
+//     {
+//       auto token = queue->push([&called]{ ++called; });
+//       THEN("it gets executed")
+//       {
+//         token.wait();
+//         REQUIRE(called == 1);
+//       }
+//     }
+//     THEN("queue can be removed")
+//     {
+//       REQUIRE(pool.remove(*queue));
+//     }
+//   }
+//   GIVEN("queue is pre-created")
+//   {
+//     auto queue = std::make_shared<decltype(pool)::queue_t>();
+//     WHEN("added (without jobs) to pool")
+//     {
+//       REQUIRE(pool.add(queue));
+//       AND_WHEN("job is pushed")
+//       {
+//         int called = 0;
+//         auto token2 = queue->push([&called]{ ++called; });
+//         THEN("it gets executed")
+//         {
+//           token2.wait();
+//           REQUIRE(called == 1);
+//         }
+//       }
+//       AND_WHEN("added to pool again")
+//       {
+//         THEN("it's not added")
+//         {
+//           REQUIRE_FALSE(pool.add(queue));
+//         }
+//       }
+//       AND_WHEN("removed from pool")
+//       {
+//         REQUIRE(pool.remove(*queue));
+//         THEN("it can be readded")
+//         {
+//           REQUIRE(pool.add(queue));
+//         }
+//       }
+//     }
+//     WHEN("added (with jobs) to pool")
+//     {
+//       THEN("existing jobs are executed")
+//       {
+//         int called = 0;
+//         auto token = queue->push([&called]{ ++called; });
+//         REQUIRE(pool.add(queue));
+//         token.wait();
+//         REQUIRE(called == 1);
+//       }
+//       AND_WHEN("removed from pool")
+//       {
+//         int called = 0;
+//         std::atomic_int latch{2};
+//         auto token1 = queue->push([&called, &latch]{ --latch; while(latch != 0); ++called; });
+//         auto token2 = queue->push([&called]{ ++called; });
+//         REQUIRE(pool.add(queue));
+//         while(latch > 1);
 
-        pool.remove(*queue);
-        REQUIRE(pool.size() == 0);
+//         pool.remove(*queue);
+//         REQUIRE(pool.size() == 0);
 
-        latch = 0;
-        token1.wait();
-        THEN("pending jobs are not executed")
-        {
-          std::this_thread::sleep_for(std::chrono::milliseconds{5});
-          REQUIRE_FALSE(token2.done());
-        }
-      }
-    }
-  }
-}
+//         latch = 0;
+//         token1.wait();
+//         THEN("pending jobs are not executed")
+//         {
+//           std::this_thread::sleep_for(std::chrono::milliseconds{5});
+//           REQUIRE_FALSE(token2.done());
+//         }
+//       }
+//     }
+//   }
+// }
 
-SCENARIO("pool: stress-test")
-{
-  static constexpr std::size_t nr_of_jobs = 1 << 16;
-  static constexpr std::size_t nr_of_threads = 4;
-  auto pool = threadable::pool<nr_of_jobs>(nr_of_threads);
-  GIVEN("pool with multiple threads")
-  {
-    WHEN("a large amount of jobs are pushed")
-    {
-      std::atomic_size_t counter{0};
-      std::vector<threadable::job_token> tokens;
-      tokens.reserve(nr_of_jobs);
+// SCENARIO("pool: stress-test")
+// {
+//   static constexpr std::size_t nr_of_jobs = 1 << 16;
+//   static constexpr std::size_t nr_of_threads = 4;
+//   auto pool = threadable::pool<nr_of_jobs>(nr_of_threads);
+//   GIVEN("pool with multiple threads")
+//   {
+//     WHEN("a large amount of jobs are pushed")
+//     {
+//       std::atomic_size_t counter{0};
+//       std::vector<threadable::job_token> tokens;
+//       tokens.reserve(nr_of_jobs);
 
-      for(std::size_t i = 0; i < nr_of_jobs; ++i)
-      {
-        tokens.push_back(pool.push([&counter]{ ++counter; }));
-      }
+//       for(std::size_t i = 0; i < nr_of_jobs; ++i)
+//       {
+//         tokens.push_back(pool.push([&counter]{ ++counter; }));
+//       }
 
-      for(const auto& token : tokens)
-      {
-        token.wait();
-      }
+//       for(const auto& token : tokens)
+//       {
+//         token.wait();
+//       }
 
-      THEN("all gets executed")
-      {
-        REQUIRE(counter.load() == nr_of_jobs);
-      }
-    }
-  }
-}
+//       THEN("all gets executed")
+//       {
+//         REQUIRE(counter.load() == nr_of_jobs);
+//       }
+//     }
+//   }
+// }
