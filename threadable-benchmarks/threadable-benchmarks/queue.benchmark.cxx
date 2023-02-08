b7f33b250dddecae743aa3058fe44ff13428571b
helmesjo <helmesjo@gmail.com>
2023-02-08 02:08:41 +0100

    queue: Remove benchmark for old implementation.

--- threadable-benchmarks/threadable-benchmarks/queue.benchmark.cxx
+++ threadable-benchmarks/threadable-benchmarks/queue.benchmark.cxx
@@ -68,31 +68,6 @@ BENCHMARK(queue_std_iterate)->Args({jobs_per_iteration})->ArgNames({"jobs"})->Us
 
 #if defined(__cpp_lib_execution) && defined(__cpp_lib_parallel_algorithm)
 
-static void queue_threadable_parallel_for(benchmark::State& state)
-{
-  const std::size_t nr_of_jobs = state.range(0);
-  auto queue = threadable::queue<jobs_per_iteration>();
-  for (auto _ : state)
-  {
-    for(std::size_t i = 0; i < nr_of_jobs; ++i)
-    {
-      queue.push([]() mutable {});
-    }
-
-    threadable::utils::time_block(state, [&]{
-      std::for_each(std::execution::par, std::begin(queue), std::end(queue), [](auto& job){
-        if(job)
-        {
-          benchmark::DoNotOptimize(job);
-          job(); // BUG: Withuot invocation here it locks in "queue::quit()". Verify destructors are called correctly.
-        }
-      });
-    });
-  }
-  state.SetItemsProcessed(nr_of_jobs * state.iterations());
-}
-BENCHMARK(queue_threadable_parallel_for)->Args({jobs_per_iteration})->ArgNames({"jobs"})->UseManualTime();
-
 static void queue2_threadable_parallel_for(benchmark::State& state)
 {
   const std::size_t nr_of_jobs = state.range(0);
