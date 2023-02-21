68a2a6c82fd4864f55462482158a874656f00729
helmesjo <helmesjo@gmail.com>
2023-02-21 02:00:05 +0100

    Remove google-bench

--- threadable-benchmarks/threadable-benchmarks/util.hxx
+++ threadable-benchmarks/threadable-benchmarks/util.hxx
@@ -1,24 +1,7 @@
 #pragma once
 
-#include <threadable/std_concepts.hxx>
-#include <benchmark/benchmark.h>
-#include <chrono>
-
 namespace threadable::utils
 {
-  template<std::invocable block_t>
-  inline void time_block(benchmark::State& state, block_t&& block)
-  {
-    auto start = std::chrono::high_resolution_clock::now();
-
-    block();
-
-    auto end = std::chrono::high_resolution_clock::now();
-    auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(
-        end - start);
-    state.SetIterationTime(elapsed_seconds.count());
-  }
-
   int do_trivial_work(int& val);
   int do_non_trivial_work(int& val);
 }
